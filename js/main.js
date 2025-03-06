69bacb0339ab0547a978e95e3c29ba1a2093fa0b
manu <164546874+emanuelmsz@users.noreply.github.com>
2025-03-06 23:46:18 +0000

    url

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const respAutor = document.getElementById('respAutor');
 
 botaoAutor.addEventListener('click', pegarInformacao);
 
-const urlBase = "https://back_end_na_vercel";
+const urlBase = "https://web-atv13-api-dnv.vercel.app/";
 
 async function pegarInformacao() {
 
